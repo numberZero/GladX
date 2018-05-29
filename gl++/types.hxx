6f4ef682fed410ce6ab8ad38958b2097ff763f3b
number Zero <silverunicorn2011@yandex.ru>
2018-05-29 17:09:13 +0300

    Works! (almost)

---
+++ gl++/types.hxx
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+
+typedef unsigned GLenum;
+typedef unsigned GLbitfield;
+typedef struct __GLsync *GLsync;
+
+typedef __ssize_t BufferOffset;
+typedef std::int32_t CheckedInt32;
+typedef GLenum ClampColorTargetARB;
+typedef GLenum ClampColorModeARB;
+typedef GLenum BufferPNameARB;
+typedef GLenum BufferPointerNameARB;
+
+typedef float ColorF;
+typedef int StencilValue;
+typedef int WinCoord;
+typedef unsigned MaskedStencilValue;
+
+typedef char const *String;
+typedef unsigned Texture;
